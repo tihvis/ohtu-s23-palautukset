22ca9ac03754d372d4db59609023ffe5a6c4fbdf
Mira <mira.tihverainen@gmail.com>
2023-11-03 17:28:31 +0200

    Week 2, task 10

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,25 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+#    print("JSON-muotoinen vastaus:")
+#    print(response)
+
+    players = []
+    nationality = "FIN"
+
+    for player_dict in response:
+        if player_dict.get('nationality') == nationality:
+            player = Player(player_dict)
+            players.append(player)
+
+    print(f"Players from {nationality}:\n")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
